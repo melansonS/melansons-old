ca4fb51ee13c09f4e7e3c7f13a9a41c179845255
Samuel Melanson <samuelmelanson@live.com>
2021-06-06 23:16:11 -0400

    finiquy mobile url search bars, link icons and contact page

--- src/components/ScrollContainer.js
+++ src/components/ScrollContainer.js
@@ -2,6 +2,9 @@ import React from "react";
 import { text } from "../utils/constants";
 import TypedText from "./TypedText";
 
+import { FaLinkedin, FaGithubSquare } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
+
 const ScrollContainer = ({ darkTheme, lang, setSection, section }) => {
   const handleScroll = (e) => {
     // forces an "even" delay when scrolling up or down before the setSection get's called
@@ -27,7 +30,7 @@ const ScrollContainer = ({ darkTheme, lang, setSection, section }) => {
         id="home"
         className={`scroll-area${darkTheme ? "--dark" : ""} ${
           section === 1 ? "current-section" : ""
-        }  first`}>
+        }  home`}>
         <TypedText
           text="Samuel Melanson. "
           subText={text.developer[lang]}
@@ -39,7 +42,7 @@ const ScrollContainer = ({ darkTheme, lang, setSection, section }) => {
         id="about"
         className={`scroll-area${darkTheme ? "--dark" : ""} ${
           section === 2 ? "current-section" : ""
-        }  second`}>
+        }  about`}>
         <h1>{text.about[lang]}</h1>
         <p>
           Lorem Ipsum is simply dummy text of the printing and typesetting
@@ -53,15 +56,45 @@ const ScrollContainer = ({ darkTheme, lang, setSection, section }) => {
         id="projects"
         className={`scroll-area${darkTheme ? "--dark" : ""} ${
           section === 3 ? "current-section" : ""
-        }  third`}>
+        }  projects`}>
         <h1>{text.projects[lang]}</h1>
       </div>
       <div
         id="contact"
         className={`scroll-area${darkTheme ? "--dark" : ""} ${
           section === 4 ? "current-section" : ""
-        }  fourth`}>
-        <h1>{text.contact[lang]}</h1>
+        }  contact`}>
+        <div>
+          <h1>Go Check out:</h1>
+          <div className="what-who">
+            <div className="contact-pitch">
+              - <b>what</b> I'm working on
+              <a
+                target="_blank"
+                href="https://github.com/melansonS"
+                rel="noreferrer">
+                <FaGithubSquare />
+              </a>
+            </div>
+            <div className="contact-pitch">
+              - <b> who </b> I'm working with
+              <a
+                target="_blank"
+                href="https://www.linkedin.com/in/samuel-melanson/"
+                rel="noreferrer">
+                <FaLinkedin />
+              </a>
+            </div>
+          </div>
+          <div className="direct-contact">
+            <div className="contact-pitch">
+              Or Get in touch with me directly!
+              <a href="mailto:samuelmelanson@live.com">
+                <HiOutlineMail className="email-icon" />
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
